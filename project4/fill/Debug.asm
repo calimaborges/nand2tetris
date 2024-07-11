60494d708490f6745583109bb56ee862c851c487
Carlos Augusto Lima Borges <calimaborges@gmail.com>
2024-07-11 11:16:10 -0300

    feat: project4 fill

---
+++ project4/fill/Debug.asm
@@ -0,0 +1,11 @@
+@SCREEN
+D=A
+@screen_addr
+M=D
+@8191
+D=D+A
+A=D
+M=-1
+@screen_addr
+M=D
+
--- project4/fill/Fill.asm
+++ project4/fill/Fill.asm
@@ -8,4 +8,60 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// debug
+@R0
+M=-1
+
+// Set @color to whatever color is in R0
+(SET_COLOR)
+  @R0
+  D=M
+  @color
+  M=D
+
+(FILL_SCREEN)
+  @SCREEN
+  D=A
+  @screen_addr
+  M=D
+  (FILL_NEXT_BLOCK)
+    // paint current block
+    @color
+    D=M
+    @screen_addr
+    A=M
+    M=D
+
+    // set screen address to next block
+    @screen_addr
+    M=M+1
+    @SCREEN
+    D=A
+    @8192
+    D=D+A
+    @screen_addr
+    D=D-M
+    @FILL_NEXT_BLOCK
+    D;JGT
+
+  @KBD
+  D=M
+  @SET_BLACK
+  D;JGT
+  @SET_WHITE
+  D;JEQ
+  @FILL_SCREEN
+  0;JMP
+
+(SET_BLACK)
+  @R0
+  M=-1
+  @SET_COLOR
+  0;JMP
+
+(SET_WHITE)
+  @R0
+  M=0
+  @SET_COLOR
+  0;JMP
+
